172836e9b5cf44c85347bb17ab8d5eec3ba3c912
Sakshi Suryawanshi <suryawanshisakshi348@gmail.com>
2026-07-22 18:53:02 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,33 +1,10 @@
-def call(Map config) {
-    pipeline {
-        agent any
-
-        stages {
-
-            stage('Build') {
-                steps {
-                    echo "Building ${config.appName}"
-                    sh 'ls -la'
-                }
-            }
-
-            stage('Test') {
-                steps {
-                    echo "Testing ${config.appName}"
-                }
-            }
-
-            stage('Scan') {
-                steps {
-                    echo "Scanning ${config.appName}"
-                }
-            }
-
-            stage('Deploy') {
-                steps {
-                    echo "Deploying ${config.appName}"
-                }
-            }
-        }
+stage('Build') {
+    steps {
+        echo "Building ${config.appName}"
+        sh '''
+            ls -la
+            test -f index.html
+            echo "Build Successful"
+        '''
     }
 }
